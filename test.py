e52b6b086cb0234e839863edfabf903465e22ea5
brianbriles <brianbriles@outlook.com>
2017-07-19 15:20:32 -0700

    more testing

--- test.py
+++ test.py
@@ -1,6 +1,7 @@
 #! /usr/bin/python3
 
 import os
+import time
 
 # Dummy vars for now
 # TODO: Make file_path user defined!
@@ -14,10 +15,18 @@ for f in os.listdir():
     file_name, file_ext = os.path.splitext(f)
     
     try:
+        mod_date = os.stat(f)
+    except IOError:
+        print('Error: Name or info of file doesn\'t exist')
+    else:
+        print("file modified: ", time.asctime(time.localtime(mod_date.st_mtime)))
+        
+
+    ''' try:
         if file_ext == '.txt':
             os.makedirs(folder_name)
     except FileExistsError:
         print('Error: Filename ' + folder_name + ' exists!')
 
     if file_ext != '':
-        print(file_ext)
+        print(file_ext) '''
