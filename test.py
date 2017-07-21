1cf70f20bc218f774193e70960c4b38c6bf3abae
brianbriles <brianbriles@outlook.com>
2017-07-21 14:05:40 -0700

    files mod date vars are working now. starting pyporg.py file.

--- test.py
+++ test.py
@@ -13,20 +13,25 @@ file_path = os.chdir(file_path)
 # Iterate through directory, splitting file name and file extension.
 for f in os.listdir():
     file_name, file_ext = os.path.splitext(f)
-    
+
+    # Grab the month the file was last modified    
     try:
-        mod_date = os.stat(f)
+        #mod_stat = os.stat(f)
+        file_stats = time.ctime(os.stat(f).st_mtime)
+        f_day, f_month, f_date, f_time, f_year = file_stats.split()
     except IOError:
         print('Error: Name or info of file doesn\'t exist')
     else:
-        print("file modified: ", time.asctime(time.localtime(mod_date.st_mtime)) + ' ' + f)
-        
+        #print("file modified: "), time.asctime(time.localtime(mod_stat.st_ctime_ns))
+        #print("file modified: " + str(mod_stat.st_ctime_ns)) #Works
+        print(f_month)
 
-    ''' try:
+        # Get rid of the nonsense and only keep the month. ex. 'Jul'
+
+    #print('DEBUG: mod_stat: ' + str(mod_stat.st_ctime_ns))
+
+    try:
         if file_ext == '.txt':
             os.makedirs(folder_name)
     except FileExistsError:
         print('Error: Filename ' + folder_name + ' exists!')
-
-    if file_ext != '':
-        print(file_ext) '''
