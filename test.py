4a2e0d628cc04a864916c4bfe39734b735123404
brianbriles <brianbriles@outlook.com>
2017-07-21 22:11:15 -0700

    adding ideas for a crude gui

--- test.py
+++ test.py
@@ -1,37 +1,59 @@
-#! /usr/bin/python3
+#!/usr/bin/python3
 
-import os
-import time
+# import the library
+from appJar import gui
 
-# Dummy vars for now
-# TODO: Make file_path user defined!
-file_path   = '/home/brian/test/PYPORG_TEST/'
-folder_name = 'MKDIR_TEST'
+# handle button events
+def press(button):
+    if button == "Close":
+        app.stop()
+    elif button == "Set Path":
+        # Set dir to work
+        app.directoryBox(title="Set Path", dirName=None)
+        # Update file_path
+    #else:
+        # Call function fileTransfer()
 
-file_path = os.chdir(file_path)
+# create a GUI variable called app
+app = gui("PyPORG", "300x300")
+app.setBg("lightgray")
 
-# Iterate through directory, splitting file name and file extension.
-for f in os.listdir():
-    file_name, file_ext = os.path.splitext(f)
+# add & configure widgets - widgets get a name, to help referencing them later
+app.addLabel("title", "Python Picture Organizer")
+app.setLabelBg("title", "lightgray")
+app.setLabelFg("title", "black")
 
-    # Grab the month the file was last modified    
-    try:
-        #mod_stat = os.stat(f)
-        file_stats = time.ctime(os.stat(f).st_mtime)
-        f_day, f_month, f_date, f_time, f_year = file_stats.split()
-    except IOError:
-        print('Error: Name or info of file doesn\'t exist')
-    else:
-        #print("file modified: "), time.asctime(time.localtime(mod_stat.st_ctime_ns))
-        #print("file modified: " + str(mod_stat.st_ctime_ns)) #Works
-        print(f_month)
+# Separator
+app.addHorizontalSeparator(1,0,4, colour="lightblue")
 
-        # Get rid of the nonsense and only keep the month. ex. 'Jul'
+# Add extension type chkboxes
+app.addCheckBox(".jpg")
+app.addCheckBox(".jpeg")
+app.addCheckBox(".png")
 
-    #print('DEBUG: mod_stat: ' + str(mod_stat.st_ctime_ns))
+app.setCheckBox(".jpg")
 
-    try:
-        if file_ext == '.txt':
-            os.makedirs(folder_name)
-    except FileExistsError:
-        print('Error: Filename ' + folder_name + ' exists!')
+# Get the status of chkboxes
+#app..getCheckBox(.jpg)...
+
+# Add delete duplicate radio
+app.addRadioButton("no_duplicate", "Save duplicate files")
+app.addRadioButton("no_duplicate", "Delete duplicate files")
+
+# Get the status of radiobtn
+#app..getRadioButton(no_duplicate)
+
+# Log
+app.setFont(12)
+app.addMessage("mess", """Ready.""")
+
+# Meter
+app.addMeter("progress")
+app.setMeterFill("progress", "blue")
+
+# link the buttons to the function called press
+app.addButtons(["Start", "Set Path", "Close"], press)
+
+
+# start the GUI
+app.go()
